0db1e85a7e91a339a7ffe44d52cab54a31966527
inker610566 <inker610566@yahoo.com.tw>
2015-05-30 18:27:16 +0800

    add parent field to the design

--- splay_tree/splay_tree.py
+++ splay_tree/splay_tree.py
@@ -4,7 +4,7 @@
 class SplayTree:
 
     def __init__(self):
-        self.ch[0] = self.ch[1] = None
+        self.ch[0] = self.ch[1] = self.p = None
         self.value = None
 
     def __getitem__(self, qValue):
@@ -28,10 +28,14 @@ class SplayTree:
             :Args:
              - lr : rotate direction, right if lr else left
         '''
+        def set_r(p, s, lr):
+            if p: p.ch[lr] = s
+            if s: s.p = p
         x = self.ch[not lr]
         assert x
-        self.ch[not lr] = x.ch[lr]
-        x.self.ch[lr] = self
+        y = x.ch[lr]
+        set_r(self, y, not lr)
+        set_r(x, self, lr)
 
 
     def _splay(self, rpath):
